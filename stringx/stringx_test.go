b66523029033a18afd340f3df075e71d21c161f7
weiwenchen2022 <weiwenchen2022@163.com>
2023-05-04 12:08:36 +0800

    third commit

--- stringx/stringx_test.go
+++ stringx/stringx_test.go
@@ -15,11 +15,9 @@ func TestRandString(t *testing.T) {
 	const n = 16
 
 	s1 := RandString(n)
-
 	if got := len(s1); got != n {
 		t.Errorf("len(s1) = %d, want %d", got, n)
 	}
-
 	for _, r := range s1 {
 		if !strings.ContainsRune(letters, r) {
 			t.Errorf("s Contains not the uppercase or lowercase letters: %q", r)
@@ -27,6 +25,15 @@ func TestRandString(t *testing.T) {
 	}
 
 	s2 := RandString(n)
+	if got := len(s2); got != n {
+		t.Errorf("len(s2) = %d, want %d", got, n)
+	}
+	for _, r := range s2 {
+		if !strings.ContainsRune(letters, r) {
+			t.Errorf("s Contains not the uppercase or lowercase letters: %q", r)
+		}
+	}
+
 	if s1 == s2 {
 		t.Error("s1 == s2")
 	}
